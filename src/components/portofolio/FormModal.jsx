0c4232e5f2cc0c7709cd7515344a02ccadd7be70
alexoltean97 <alex.oltean97@gmail.com>
2024-01-22 18:41:38 +0200

    Initial portal setups

---
+++ src/components/portofolio/FormModal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {createPortal} from 'react-dom';
+
+const FormModal = ({isOpen, onClose}) => {
+    if(!isOpen) return null;
+    return(
+
+        <p>mata</p>
+    );
+}
+
+export default FormModal;
---
+++ src/components/utils/ReactPortal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {createPortal} from 'react-dom';
+
+const Modal = ({isOpen, onClose, children}) => {
+    if(!isOpen) return null;
+
+    return createPortal(
+        <div className="modal" tabindex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              {children}
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary" onClick={onClose}>Save changes</button>
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+        ,
+        document.getElementById('modal')
+    )
+}
+
+export default Modal;

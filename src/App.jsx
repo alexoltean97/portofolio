1cee154854c7bc27bf84716136108f54a0b9db64
alexoltean97 <alex.oltean97@gmail.com>
2024-01-30 17:41:30 +0200

    Portals

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 
 const App = () => {
-  useEffect(() => {
-    // Select the elements description
-    const h1 = document.getElementById("education-subtitle");
-    const p =  document.getElementById("education");
+  // useEffect(() => {
+  //   // Select the elements description
+  //   const h1 = document.getElementById("education-subtitle");
+  //   const p =  document.getElementById("education");
 
-    // Get their positions
-    const h1Rect = h1.getBoundingClientRect();
-    const pRect = p.getBoundingClientRect();
+  //   // Get their positions
+  //   const h1Rect = h1.getBoundingClientRect();
+  //   const pRect = p.getBoundingClientRect();
 
-    // Calculate distances
-    const verticalDistance = Math.abs(pRect.top - h1Rect.bottom);
-    const horizontalDistance = Math.abs(pRect.left - h1Rect.right);
+  //   // Calculate distances
+  //   const verticalDistance = Math.abs(pRect.top - h1Rect.bottom);
+  //   const horizontalDistance = Math.abs(pRect.left - h1Rect.right);
 
-    // Log the distances
-    console.log(`Vertical distance: ${verticalDistance}px`);
-    console.log(`Horizontal distance: ${horizontalDistance}px`);
+  //   // Log the distances
+  //   console.log(`Vertical distance: ${verticalDistance}px`);
+  //   console.log(`Horizontal distance: ${horizontalDistance}px`);
 
 
-    const work = document.getElementById("work-description").innerText.length;
-    console.log(work);
-  }),
-    [];
+  //   const work = document.getElementById("work-description").innerText.length;
+  //   console.log(work);
+  // }),
+  //   [];
 
   return (
     <Router>

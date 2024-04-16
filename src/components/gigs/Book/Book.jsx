f6c51643c315fe95deb9d2495d1f2ae2eef2a967
alexoltean97 <alex.oltean97@gmail.com>
2024-04-16 19:34:37 +0300

    Toxic

--- src/components/gigs/Book/Book.jsx
+++ src/components/gigs/Book/Book.jsx
@@ -3,8 +3,16 @@ import React from "react";
 const Book = () => {
   return (
     <div>
-      <a href="#">Book on fiver.</a>
-      <a href="#">Schedule a metting.</a>
+      <a
+        className="btn-gigs btn-fiver"
+        href="https://www.fiverr.com/alexoltean?up_rollout=true"
+        target="_new"
+      >
+        Book on fiver.
+      </a>
+      <a className="btn-gigs btn-email" href="#">
+        Schedule a metting.
+      </a>
     </div>
   );
 };

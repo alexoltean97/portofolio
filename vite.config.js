d1d59e99cb3b793a7217cfd2b7de7784b8021300
alexoltean97 <alex.oltean97@gmail.com>
2024-04-25 10:28:59 +0300

    Config again

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: 'assets', 
-    rollupOptions: {
-      output: {
-        assetFileNames: assetInfo => {
-          return `${assetInfo.name}`
-        }
-      }
-    }
-  }
+  
 })
